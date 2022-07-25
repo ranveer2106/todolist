c0917c2639fe6aca0b8139822d5f6dffb1455af9
ranvir <ranveerbhatti2106@gmail.com>
2022-07-25 10:54:18 +0530

    this is a to do list app

---
+++ index.js
@@ -0,0 +1,56 @@
+document.querySelector('#sinput').onclick = function () {
+
+
+    if (document.querySelector('#input').value == '') {
+        alert("please enter something");
+    }
+    else {
+        // e.preventDefault();
+        document.querySelector('#tasklist').innerHTML +=
+            `
+        <div class="taskele">
+            <div class="content">
+            ${document.querySelector('#input').value}
+            </div>
+            <button class="complete">
+            <i class="fa-solid fa-check fa-2x"></i>
+            </button>
+            <button class="delete">
+                <i class="fa-solid fa-trash-can fa-2x"></i>
+            </button>
+        </div>
+        `;
+
+
+        let tasknow = document.querySelectorAll(".delete");
+        for (let i = 0; i < tasknow.length; i++) {
+            tasknow[i].onclick = function () {
+                this.parentNode.remove();
+            }
+
+        }
+        let complete = document.querySelectorAll(".taskele");
+        for (let k = 0; k < complete.length; k++) {
+            complete[k].onclick = function () {
+                this.classList.toggle('completed');
+            }
+
+        }
+        // let tasks = document.querySelectorAll(".taskele");
+        // for (let i = 0; i < tasks.length; i++) {
+        //     tasks[i].onclick = function () {
+        //         this.classList.toggle('completed');
+        //     }
+        // }
+        document.querySelector('#input').value = "";
+    }
+}
+
+// let tasknow = document.querySelectorAll(".delete");
+// for (let i = 0; i < tasknow.length; i++) {
+//     tasknow[i].onclick = function () {
+//         // this.parentNode.removeChild();
+//         console.log('h');
+//     }
+// }
+
